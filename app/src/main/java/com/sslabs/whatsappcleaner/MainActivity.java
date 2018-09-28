6d934f49a46cb6955151f962e2f9fafa73b72399
Plinio Sandes dos Santos <ss.plinio@gmail.com>
2018-09-28 19:03:31 -0300

    Add patter to delete old backup databases

--- app/src/main/java/com/sslabs/whatsappcleaner/MainActivity.java
+++ app/src/main/java/com/sslabs/whatsappcleaner/MainActivity.java
@@ -1,48 +1,60 @@
 package com.sslabs.whatsappcleaner;
 
 import android.Manifest;
-import android.content.ContentResolver;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DATABASES_PATH = "WhatsApp/Databases";
-    private static final String FILE_NAME = DATABASES_PATH + "/msgstore-2018-09-15.1.db.crypt12";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ensurePermissions();
+    }
+
+    public void onDeleteDatabasesClick(View view) {
+        deleteBackupDatabases();
+    }
+
+    private void deleteBackupDatabases() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File databasesPath = new File(Environment.getExternalStorageDirectory(), DATABASES_PATH);
+            File[] backupDatabases = databasesPath.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return Pattern.matches("^msgstore-.*\\.db\\.crypt12$", pathname.getName());
+                }
+            });
+//            if (dbFile.exists()) {
+//                dbFile.delete();
+//            }
+        }
+    }
+
+    private void ensurePermissions() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_CONTACTS)) {
-
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                //TODO: how should it behave?
             } else {
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         0);
-            }
-        }
-    }
-
-    public void onDeleteDatabasesClick(View view) {
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            File dbFile = new File( Environment.getExternalStorageDirectory(), FILE_NAME);
-            if (dbFile.exists()) {
-                dbFile.delete();
             }
         }
     }

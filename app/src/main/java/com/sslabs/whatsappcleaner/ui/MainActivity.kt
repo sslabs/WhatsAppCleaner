16fba983c01993f23790fb37392770df6834b873
Plinio Sandes dos Santos <ss.plinio@gmail.com>
2020-01-16 16:38:12 -0300

    Add schedule card layout

---
+++ app/src/main/java/com/sslabs/whatsappcleaner/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sslabs.whatsappcleaner.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sslabs.whatsappcleaner.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
